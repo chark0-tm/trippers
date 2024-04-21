f3f2d95fb059cff100a2348894b6bcb37f07ac90
chark0-tm <ltri@themachines.ca>
2024-04-21 08:18:36 -0400

    modify js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-if (window.DeviceOrientationEvent) {
-    window.addEventListener('deviceorientation', function(event) {
-        var alpha = event.alpha;
-        var compassImg = document.getElementById('compass');
-        compassImg.style.transform = 'rotate(' + (-alpha) + 'deg)';
-    }, false);
-} else {
-    alert("Your device does not support orientation sensors");
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var compassImg = document.getElementById('compass');
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener('deviceorientation', function(event) {
+            var alpha;
+            // Check if event.alpha is available
+            if (event.webkitCompassHeading) {
+                // Apple devices use webkitCompassHeading
+                alpha = event.webkitCompassHeading;
+                // Correcting the angle of the compass image
+                compassImg.style.transform = 'rotate(' + (-alpha) + 'deg)';
+            } else if (event.alpha != null) {
+                // Non-Apple devices
+                alpha = event.alpha;
+                compassImg.style.transform = 'rotate(' + alpha + 'deg)';
+            } else {
+                alert("Compass not supported");
+            }
+        }, false);
+    } else {
+        alert("Device orientation not supported");
+    }
+});
